8ba418a17a29183fa84b7f1cb3da8f068229f33e
codezjx <code.zjx@gmail.com>
2017-10-16 22:31:15 +0800

    Add Flowable support for RxJava2.

--- src/main/java/com/codezjx/linker/adapter/rxjava2/RxJava2CallAdapter.java
+++ src/main/java/com/codezjx/linker/adapter/rxjava2/RxJava2CallAdapter.java
@@ -3,26 +3,33 @@ package com.codezjx.linker.adapter.rxjava2;
 import com.codezjx.linker.Call;
 import com.codezjx.linker.CallAdapter;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
 
 /**
  * Created by codezjx on 2017/10/14.<br/>
  */
-final class RxJava2CallAdapter<R> implements CallAdapter<R, Observable<R>> {
+final class RxJava2CallAdapter<R> implements CallAdapter<R, Object> {
 
     private final Scheduler mScheduler;
+    private final boolean mIsFlowable;
 
-    RxJava2CallAdapter(Scheduler scheduler) {
+    RxJava2CallAdapter(Scheduler scheduler, boolean isFlowable) {
         mScheduler = scheduler;
+        mIsFlowable = isFlowable;
     }
 
     @Override
-    public Observable<R> adapt(Call<R> call) {
+    public Object adapt(Call<R> call) {
         Observable<R> observable = new CallExecuteObservable<>(call);
 
         if (mScheduler != null) {
             observable = observable.subscribeOn(mScheduler);
+        }
+
+        if (mIsFlowable) {
+            return observable.toFlowable(BackpressureStrategy.LATEST);
         }
         
         return observable;
--- src/main/java/com/codezjx/linker/adapter/rxjava2/RxJava2CallAdapterFactory.java
+++ src/main/java/com/codezjx/linker/adapter/rxjava2/RxJava2CallAdapterFactory.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
 
@@ -32,16 +33,18 @@ public class RxJava2CallAdapterFactory extends CallAdapter.Factory {
     @Override
     public CallAdapter<?, ?> get(Type returnType, Annotation[] annotations) {
         Class<?> rawType = getRawType(returnType);
-        if (rawType != Observable.class) {
+        boolean isFlowable = (rawType == Flowable.class);
+        if (rawType != Observable.class && !isFlowable) {
             return null;
         }
 
         if (!(returnType instanceof ParameterizedType)) {
-            throw new IllegalStateException("Observable return type must be parameterized"
-                    + " as Observable<Foo> or Observable<? extends Foo>");
+            String name = isFlowable ? "Flowable" : "Observable";
+            throw new IllegalStateException(name + " return type must be parameterized"
+                    + " as " + name + "<Foo> or " + name + "<? extends Foo>");
         }
         
-        return new RxJava2CallAdapter(mScheduler);
+        return new RxJava2CallAdapter(mScheduler, isFlowable);
     }
     
 }
