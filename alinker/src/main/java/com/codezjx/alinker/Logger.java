839400b350f83afe304c5f0fcfb61096afd36fd3
codezjx <code.zjx@gmail.com>
2018-01-24 23:30:00 +0800

    Improve logger, add lazy method for calling without TAG.

--- alinker/src/main/java/com/codezjx/alinker/Logger.java
+++ alinker/src/main/java/com/codezjx/alinker/Logger.java
@@ -1,53 +1,88 @@
 package com.codezjx.alinker;
 
+import android.os.Build;
 import android.util.Log;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by codezjx on 2018/1/23.<br/>
  */
 public final class Logger {
-    
+
+    private static final String DEFAULT_LOG_TAG = "Logger";
+    private static final int MAX_TAG_LENGTH = 23;
+    // 3 method calls inside Logger
+    private static final int CALL_STACK_INDEX = 3;
+    private static final Pattern ANONYMOUS_CLASS = Pattern.compile("(\\$\\d+)+$");
     static boolean sEnable = true;
 
+    public static void v(String msg) {
+        log(Log.VERBOSE, null, msg, null);
+    }
+
     public static void v(String tag, String msg) {
-        if (sEnable) {
-            Log.v(tag, msg);
-        }
+        log(Log.VERBOSE, tag, msg, null);
+    }
+
+    public static void d(String msg) {
+        log(Log.DEBUG, null, msg, null);
     }
     
     public static void d(String tag, String msg) {
-        if (sEnable) {
-            Log.d(tag, msg);
-        }
+        log(Log.DEBUG, tag, msg, null);
+    }
+
+    public static void i(String msg) {
+        log(Log.INFO, null, msg, null);
     }
 
     public static void i(String tag, String msg) {
-        if (sEnable) {
-            Log.i(tag, msg);
-        }
+        log(Log.INFO, tag, msg, null);
+    }
+
+    public static void w(String msg) {
+        log(Log.WARN, null, msg, null);
     }
 
     public static void w(String tag, String msg) {
-        if (sEnable) {
-            Log.w(tag, msg);
-        }
+        log(Log.WARN, tag, msg, null);
+    }
+
+    public static void w(String msg, Throwable t) {
+        log(Log.WARN, null, msg, t);
     }
 
     public static void w(String tag, String msg, Throwable t) {
-        w(tag, msg + '\n' + getStackTraceString(t));
+        log(Log.WARN, tag, msg, t);
+    }
+
+    public static void e(String msg) {
+        log(Log.ERROR, null, msg, null);
     }
 
     public static void e(String tag, String msg) {
-        if (sEnable) {
-            Log.e(tag, msg);
-        }
+        log(Log.ERROR, tag, msg, null);
+    }
+
+    public static void e(String msg, Throwable t) {
+        log(Log.ERROR, null, msg, t);
     }
 
     public static void e(String tag, String msg, Throwable t) {
-        e(tag, msg + '\n' + getStackTraceString(t));
+        log(Log.ERROR, tag, msg, t);
+    }
+
+    private static void log(int priority, String tag,  String msg, Throwable t) {
+        if (!sEnable) {
+            return;
+        }
+        String curTag = (tag != null && tag.trim().length() > 0) ? tag : getTag();
+        String curMsg = (t != null) ? (msg + '\n' + getStackTraceString(t)) : msg;
+        Log.println(priority, curTag, curMsg);
     }
     
     private static String getStackTraceString(Throwable t) {
@@ -61,5 +96,28 @@ public final class Logger {
         t.printStackTrace(pw);
         pw.flush();
         return sw.toString();
+    }
+
+    private static String getTag() {
+        StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+        if (stackTrace.length <= CALL_STACK_INDEX) {
+            Log.e(DEFAULT_LOG_TAG, "Synthetic stacktrace didn't have enough elements, use the default logger tag, are you using proguard?");
+            return DEFAULT_LOG_TAG;
+        }
+        return createStackElementTag(stackTrace[CALL_STACK_INDEX]);
+    }
+
+    private static String createStackElementTag(StackTraceElement element) {
+        String tag = element.getClassName();
+        Matcher m = ANONYMOUS_CLASS.matcher(tag);
+        if (m.find()) {
+            tag = m.replaceAll("");
+        }
+        tag = tag.substring(tag.lastIndexOf('.') + 1);
+        // Tag length limit was removed in API 24.
+        if (tag.length() <= MAX_TAG_LENGTH || Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return tag;
+        }
+        return tag.substring(0, MAX_TAG_LENGTH);
     }
 }
